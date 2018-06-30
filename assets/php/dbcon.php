d85cb69d57e92683325ed4a1d381087b60615ade
lavish kumar jangir <lavishkumar12@gmail.com>
2018-07-01 01:51:57 +0530

    Update dbcon.php

--- assets/php/dbcon.php
+++ assets/php/dbcon.php
@@ -1,9 +1,21 @@
 <?php
 
-$conn = mysqli_connect('database-server-azure.mysql.database.azure.com','prashasy@database-server-azure','TrueStory@786');
+// $conn = mysqli_connect('database-server-azure.mysql.database.azure.com','prashasy@database-server-azure','TrueStory@786');
 
-        if (!$conn) {
-            die("Connection failed: " . $conn);
-        } 
+//         if (!$conn) {
+//             die("Connection failed: " . $conn);
+//         } 
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:tiedc-azure.database.windows.net,1433; Database = tiedc", "tiedc-admin", "TrueStory@786");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+
+
 
 ?>
