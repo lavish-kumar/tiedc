3f243b1c20f579c12fd0d4123754d5726f647001
lavish kumar jangir <lavishkumar12@gmail.com>
2018-07-01 01:05:51 +0530

    Update dbcon.php

--- assets/php/dbcon.php
+++ assets/php/dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','tiedc');
+$conn = mysqli_connect('database-server-azure.mysql.database.azure.com','prashasy@database-server-azure','TrueStory@786');
 
         if (!$conn) {
             die("Connection failed: " . $conn);
