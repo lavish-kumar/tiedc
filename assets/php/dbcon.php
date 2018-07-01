559197606437e32ee95f6379cc6fbbe65008b343
lavish kumar jangir <lavishkumar12@gmail.com>
2018-07-01 12:32:37 +0530

    Update dbcon.php

--- assets/php/dbcon.php
+++ assets/php/dbcon.php
@@ -7,7 +7,7 @@
 //         } 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:tiedc-azure.database.windows.net,1433; Database = tiedc", "tiedc-admin", "TrueStory@786");
+    $conn = new PDO("sqlsrv:server = tiedc-azure.mysql.database.azure.com; Database = tiedc", "tiedc@tiedc-azure", "TrueStory@786");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
